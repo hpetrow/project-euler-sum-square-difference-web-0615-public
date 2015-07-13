031659edb5896cb5f901f6dbbd6d42e406ecf7c4
hpetrow <heather.petrow@gmail.com>
2015-07-13 09:33:51 -0400

    done

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+
+class SumSquareDifference
+  attr_reader :difference
+
+  def initialize(n)
+    @difference = sum_square_difference(n)
+  end
+
+def sum_square_difference(n)
+  (sum_squares(n) - square_sum(n)).abs
+end
+
+def sum_squares(n)
+  (1..n).inject(0) { |s, n|
+    s + n*n
+  }
+end
+
+def square_sum(n)
+  sum = (1..n).inject(0) { |s, n|
+    s + n
+  }
+  sum * sum
+end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+def sum_square_difference(n)
+  (sum_squares(n) - square_sum(n)).abs
+end
+
+def sum_squares(n)
+  (1..n).inject(0) { |s, n|
+    s + n*n
+  }
+end
+
+def square_sum(n)
+  sum = (1..n).inject(0) { |s, n|
+    s + n
+  }
+  sum * sum
+end
